64d9f864fcb78e98a5149420a16ad8d7a0e7da64
karan <karansharma1395@gmail.com>
2023-02-22 14:34:59 +0530

    Added ViewSet

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from profiles_api.views import HelloApiView
+from profiles_api.views import HelloApiView, HelloViewSet
+
+router = DefaultRouter()
+# this will create the full url for us so we do not need to specify /
+router.register('hello_viewset', HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello/', HelloApiView.as_view()),
+    # as we register new routes with router it generates list of all urls for our viewset
+    path('', include(router.urls))
 ]
+
+# to register ViewSets we use router that is a class provided by drf in order to generate diff router available for our viewSet.
+#  
